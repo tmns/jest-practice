fe33d16461d3a8d3230b2f297e9bdbd99c6258e4
sairim <spiri@protonmail.com>
2019-08-11 16:20:53 +0300

    add test that simulates click counter inc & dec button

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { mount } from 'enzyme';
 
 import App, { Counter, dataReducer } from "./App";
 
@@ -41,6 +42,42 @@ describe("App", () => {
     const component = renderer.create(<App />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the inner Counter', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Counter).length).toEqual(1);
+  });
+
+  it('passes all props to Counter', () => {
+    const wrapper = mount(<App />);
+    const counterWrapper = wrapper.find(Counter);
+
+    expect(counterWrapper.find('p').text()).toEqual('0');
+  });
+
+  it('increments the counter', () => {
+    const wrapper = mount(<App />);
+
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toBe('1');
+  });
+
+  it('decrements the counter', () => {
+    const wrapper = mount(<App />);
+
+    wrapper
+      .find('button')
+      .at(1)
+      .simulate('click');
+
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toBe('-1');
   });
 });
 
