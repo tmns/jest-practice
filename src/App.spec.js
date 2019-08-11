2edbb050c1e4f2c9bcc0b5d6e5f017d0d6aaa418
sairim <spiri@protonmail.com>
2019-08-11 13:40:11 +0300

    add snapshot test example

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,20 @@
-describe("My Test Suite", () => {
-  it("My Test Case", () => {
-    expect(true).toEqual(true);
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App, { Counter } from "./App";
+
+describe("App", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Counter", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<Counter counter={1} />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
